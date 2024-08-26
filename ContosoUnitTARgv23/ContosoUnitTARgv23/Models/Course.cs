22059e926d69006fc464020af8a3c40113a25b07
xideme <soopka@gmail.com>
2024-08-26 16:10:08 +0300

    Models

---
+++ ContosoUnitTARgv23/ContosoUnitTARgv23/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace ContosoUnitTARgv23.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUnitTARgv23/ContosoUnitTARgv23/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿namespace ContosoUnitTARgv23.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentID { get; set; }
+        public Grade Grade { get; set; }
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, E, F,
+    }
+}
